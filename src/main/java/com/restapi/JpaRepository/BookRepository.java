ed774cfb3fabdda4a4fd288724a7efa67b068cee
thanhhai273 <54071485+thanhhai273@users.noreply.github.com>
2021-03-01 20:25:45 +0700

    Update BookRepository.java

--- src/main/java/com/restapi/JpaRepository/BookRepository.java
+++ src/main/java/com/restapi/JpaRepository/BookRepository.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import javax.persistence.Table;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +13,11 @@ import com.restapi.model.Book;
 
 @Table(name="books")
 @Repository
-public interface BookRepository extends CrudRepository<Book, String>{
+public interface BookRepository extends JpaRepository<Book, Long>{
 	@Query("select s from books s where s.name like %?1%")
-	List<Book> findByName(String name);
+	List<Book> findByName(@Param("name") String name);
+	
 
 }
+
 
