fcef9c717e3adf258e5ecaf05cf4f9704f50907a
thanhhai273 <54071485+thanhhai273@users.noreply.github.com>
2021-03-01 20:32:01 +0700

    Update restful-api.sql

--- restful-api.sql
+++ restful-api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 28, 2021 lúc 03:58 AM
+-- Thời gian đã tạo: Th3 01, 2021 lúc 02:30 PM
 -- Phiên bản máy phục vụ: 10.4.14-MariaDB
 -- Phiên bản PHP: 7.4.11
 
@@ -33,16 +33,38 @@ CREATE TABLE `authors` (
   `age` int(10) NOT NULL,
   `birth` date NOT NULL,
   `phone` bigint(20) NOT NULL,
-  `email` varchar(30) NOT NULL
+  `email` varchar(30) NOT NULL,
+  `numberOfBook` int(20) NOT NULL,
+  `number_of_book` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `authors`
 --
 
-INSERT INTO `authors` (`id`, `name`, `age`, `birth`, `phone`, `email`) VALUES
-(1, 'tuan', 21, '2021-02-03', 0, 'asdas@gmail.com'),
-(1, 'tuan', 21, '2021-02-03', 0, 'asdas@gmail.com');
+INSERT INTO `authors` (`id`, `name`, `age`, `birth`, `phone`, `email`, `numberOfBook`, `number_of_book`) VALUES
+(1, 'oda', 21, '2021-03-24', 1635416541, 'thanhhai@gmail.com', 0, 0),
+(2, 'fujiko', 23, '2021-03-12', 1635416541, 'aasd@gmail.com', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `author_book`
+--
+
+CREATE TABLE `author_book` (
+  `author_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `author_book`
+--
+
+INSERT INTO `author_book` (`author_id`, `book_id`) VALUES
+(1, 1),
+(1, 3),
+(2, 2);
 
 -- --------------------------------------------------------
 
@@ -65,9 +87,9 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `name`, `language`, `category`, `author`, `quantity`, `publishing`) VALUES
-(1, 'conan', 'viet nam', 'trinh tham', 'edogawa', 20, '2021-02-17'),
-(27, 'doraemon', 'viet nam', 'thieu nhi', 'fujiko', 100, '2021-04-17'),
-(31, 'onepiece', 'viet nam', 'phieu luu', 'oda sensei', 20, '2021-02-17');
+(1, 'onepiece', 'viet nam', 'hanh dong', 'oda', 20, '2021-02-17'),
+(2, 'doraemon', 'viet nam', 'thieu nhi', 'fujiko', 10, '2021-02-17'),
+(3, 'bleach', 'viet nam', 'hanh dong', 'oda', 20, '2021-03-03');
 
 -- --------------------------------------------------------
 
@@ -84,19 +106,47 @@ CREATE TABLE `hibernate_sequence` (
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(32);
+(37);
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
+-- Chỉ mục cho bảng `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+
+--
+-- Chỉ mục cho bảng `author_book`
+--
+ALTER TABLE `author_book`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `FK7cqs8nb7l859jcwwqoawcokqf` (`author_id`);
+
+--
 -- Chỉ mục cho bảng `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`),
+  ADD KEY `author` (`author`);
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `author_book`
+--
+ALTER TABLE `author_book`
+  ADD CONSTRAINT `FK7cqs8nb7l859jcwwqoawcokqf` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `FKmeehr164a2cpxegeiawuv40a3` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
