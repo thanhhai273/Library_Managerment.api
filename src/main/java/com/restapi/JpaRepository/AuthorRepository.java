bb8a3bf84a35860451711a3614cdd42572a8cc78
thanhhai273 <54071485+thanhhai273@users.noreply.github.com>
2021-03-01 20:25:11 +0700

    Update AuthorRepository.java

--- src/main/java/com/restapi/JpaRepository/AuthorRepository.java
+++ src/main/java/com/restapi/JpaRepository/AuthorRepository.java
@@ -2,17 +2,15 @@ package com.restapi.JpaRepository;
 
 import java.util.List;
 
-import javax.persistence.Table;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.restapi.model.Author;
 
-@Table(name="authors")
 @Repository
-public interface AuthorRepository extends CrudRepository<Author, String>{
+public interface AuthorRepository extends JpaRepository<Author, Long>{
 	@Query("select s from authors s where s.name like %?1%")
-	List<Author> findByName(String name);
+	List<Author> findByName(@Param("name") String name);
 }
