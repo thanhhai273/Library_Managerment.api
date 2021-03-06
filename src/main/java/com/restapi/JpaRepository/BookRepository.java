2be54dc6c02ffccf0a53ddde3616da0f26147dbf
thanhhai273 <54071485+thanhhai273@users.noreply.github.com>
2021-03-06 09:49:30 +0700

    Update BookRepository.java

--- src/main/java/com/restapi/JpaRepository/BookRepository.java
+++ src/main/java/com/restapi/JpaRepository/BookRepository.java
@@ -11,13 +11,16 @@ import org.springframework.stereotype.Repository;
 
 import com.restapi.model.Book;
 
-@Table(name="books")
+@Table(name = "books")
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends JpaRepository<Book, Long> {
 	@Query("select s from books s where s.name like %?1%")
-	List<Book> findByName(@Param("name") String name);
-	
+	Book findByName(@Param("name") String name);
+
+	@Query("select a.id,count(b.name) as numberOfBook from books b, authors a where a.id =?1 and a.name=b.author group by b.author ")
+	List<NumberOfBook> findAllBy(Long id);
 
 }
+
 
 
